fda584b9f15c862ab81aaca8fefbf7ccdd1d0a38
Haley <Haley.boyles@opsknights.com>
2015-10-02 08:39:56 -0400

    yay

--- src/Countdown.java
+++ src/Countdown.java
@@ -4,15 +4,24 @@
 public class Countdown {
 
     public static String pattern( int n){
-        //Happy Coding ^_^
-        return "";
-    }
+    	String sting = "";
+	    for(int j=n; j>0; j--){
+			for(int i=j; i>0; i--){
+				sting=sting+i;
+			}
+			sting=sting+"\n";
+		}
+		return sting;
+		
+		
+	}
+	
 
     public static void main(String[] args){
 
         System.out.println("Hello");
         System.out.println("You can use this method to test your work and see output");
-        System.out.println("Don't forget to also use the Tests");
-        System.out.print(pattern(4));
+        System.out.println();
+        System.out.print(pattern(10));
     }
 }
